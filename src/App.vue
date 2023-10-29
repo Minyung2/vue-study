53421afba2f4478dcf812fd45069ed65d8114722
Minyung2 <ningashong@gmail.com>
2023-10-29 13:01:19 +0900

    뷰 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>허위매물 뷰동산</h1>
+    <h4 class="room1" :style="스타일">{{ products.at(0) }}</h4>
+    <p>{{ prices.at(0) }}</p>
+  </div>
+  <div>
+    <h1>허위매물 뷰동산</h1>
+    <h4 class="room1" :style="스타일">{{ products.at(1) }}</h4>
+    <p>{{ prices.at(1) }}</p>
+  </div>
+  <div>
+    <h1>허위매물 뷰동산</h1>
+    <h4 class="room1" :style="스타일">{{ products.at(2) }}</h4>
+    <p>{{ prices.at(2) }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      prices : [100, 40, 80],
+      스타일 : 'color:blue'
+    }
+  },
+  components: {}
 }
 </script>
 
