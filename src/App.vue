eab5ed783b3556e6de92cf80bf84d164e5e74368
Minyung2 <ningashong@gmail.com>
2023-10-29 14:45:51 +0900

    Modal

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,40 @@
   <div class="menu">
     <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
+
+  <div class="black-bg" v-if="changeModal==true">
+    <div class="white-bg">
+      <button class="closeModal" @click="changeModal=false">X</button>
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+    </div>
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>허위매물 뷰동산</h1>
-  <div>
+<!--  <div @click="changeModal=true">
+    <img src="./assets/room0.jpg" class="room-img">
     <h4 class="rooms" :style="스타일">{{ products[0] }}</h4>
     <p>{{ prices[0] }} 만원</p>
     <button @click="increaseReport(0)">허위매물신고</button>
     <span>신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4 class="rooms" :style="스타일">{{ products[1] }}</h4>
     <p>{{ prices.at(1) }} 만원</p>
     <button @click="increaseReport(1)">허위매물신고</button>
     <span>신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4 class="rooms" :style="스타일">{{ products[2] }}</h4>
     <p>{{ prices.at(2) }} 만원</p>
     <button @click="increaseReport(2)">허위매물신고</button>
     <span>신고수 : {{ 신고수[2] }}</span>
-  </div>
-  <div v-for="(a,i) in products" :key="i">
+  </div>-->
+  <div v-for="(a,i) in products" :key="i" @click="changeModal=true">
+    <img :src="require(`@/assets/room${i}.jpg`)" class="room-img">
     <h4 class="rooms" :style="스타일">{{ products[i] }}</h4>
     <p>{{ prices[i] }} 만원</p>
     <button @click="increaseReport(i)">허위매물신고</button>
@@ -38,7 +51,8 @@ export default {
   name: 'App',
   data() {
     return {
-      신고수: [0,0,0],
+      changeModal: false,
+      신고수: [0, 0, 0],
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
       prices: [100, 40, 80],
@@ -64,6 +78,41 @@ export default {
   color: #2c3e50;
 }
 
+.closeModal {
+  border-radius: 5px;
+  background: transparent;
+  border-color: transparent;
+  font-size: 30px;
+  margin-rigt: 100px;
+  align-self: flex-end;
+}
+
+body {
+  margin: 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
 .menu {
   background-color: darkslateblue;
   padding: 15px;
@@ -73,5 +122,10 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 50%;
+  margin-top: 50px;
 }
 </style>
